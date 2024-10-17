daf0aeef6a975787ad92dfd46d09f1fa480f2edb
meleksguner <msenaguner34@gmail.com>
2024-10-17 22:38:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,178 @@
+﻿
+//İki sayının farkını bulan c# kodu
+
+
+//using System;
+//class Program
+//{
+//    static void Main()
+//    {
+
+//        Console.Write("Birinci sayıyı girin: ");
+//        int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+//        Console.Write("İkinci sayıyı girin: ");
+//        int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+//        int fark = sayi1 - sayi2;
+
+//        Console.WriteLine($"İki sayının farkı:{fark}");
+//    }
+
+//}
+
+
+
+
+
+//5 tane sayının karesini bulan c# kodu
+
+//using System;
+
+//class program
+//{
+//    static void Main()
+//    {
+//        Console.Write("Birinci sayıyı giriniz: ");
+//        double sayi1 = Convert.ToDouble(Console.ReadLine());
+
+//        Console.Write("İkinci sayıyı giriniz:");
+//        double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+//        Console.Write("Üçüncü sayıyı giriniz: ");
+//        double sayi3 = Convert.ToDouble(Console.ReadLine());
+
+//        Console.Write("Dördüncü sayıyı giriniz: ");
+//        double sayi4 = Convert.ToDouble(Console.ReadLine());
+
+//        Console.Write("Beşinci sayıyı giriniz: ");
+//        double sayi5 = Convert.ToDouble(Console.ReadLine());
+
+
+//        Console.WriteLine("\nGirilen sayıların kareleri:");
+//        Console.WriteLine("Birinci sayının karesi: " + KaresiniAl(sayi1));
+//        Console.WriteLine("İkinci sayının karesi: " + KaresiniAl(sayi2));
+//        Console.WriteLine("Üçüncü sayının karesi: " + KaresiniAl(sayi3));
+//        Console.WriteLine("Dördüncü sayının karesi: " + KaresiniAl(sayi4));
+//        Console.WriteLine("Beşinci sayının karesi: " + KaresiniAl(sayi5));
+
+//    }
+//    static double KaresiniAl(double sayi)
+//    {
+//        return sayi * sayi;
+//    }
+//}
+
+
+
+
+
+
+
+// 10 sayısının 3'e bölümünden kalanını bulan c# kodu
+
+//using System;
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("10 sayısının 3'e bölümünden kalan: " + (10 % 3));
+//    }
+//}
+
+
+
+
+
+
+
+//4 tane sayının toplamını ve çarpımını bulan c# kodu
+
+//using System;
+//class Program
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("1.sayıyı girin: ");
+//        int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+//        Console.WriteLine("2.sayıyı girin: ");
+//        int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+//        Console.WriteLine("3.sayıyı girin: ");
+//        int sayi3 = Convert.ToInt32(Console.ReadLine());
+
+//        Console.WriteLine("4.sayıyı girin: ");
+//        int sayi4 = Convert.ToInt32(Console.ReadLine());
+
+//        int toplam = sayi1 + sayi2 + sayi3 + sayi4;
+//        int carpim = sayi1 * sayi2 * sayi3 * sayi4;
+
+//        Console.WriteLine("Girilen sayıların toplamı: " + toplam);
+//        Console.WriteLine("Girilen sayıların çarpımı: " + carpim);
+
+//    }
+//}
+
+
+
+
+
+
+//iki sayının bölümünü bulan c# kodu
+
+//using System;
+
+//class Program
+//{
+
+//    static void Main()
+//    {
+//        Console.WriteLine("Bir sayı giriniz: ");
+//        int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+//        Console.WriteLine("Bir sayi giriniz: ");
+//        int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+//        Console.WriteLine("İki sayının bölümünün sonucu: " + sayi1 / sayi2);
+
+
+//    }
+//}
+
+
+
+
+
+
+
+// Dikdörtgenler prizmasının hacmini bulan c# kodu
+
+//using System;
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("Dikdörtgenler prizmasının birinci kenarını girin: ");
+//        float a = Convert.ToInt32(Console.ReadLine());
+
+//        Console.WriteLine("Dikdörtgenler prizmasının ikinci kenarını girin: ");
+//        float b = Convert.ToInt32(Console.ReadLine());
+
+//        Console.WriteLine("Dikdörtgenler prizmasının üçüncü kenarını girin: ");
+//        float c = Convert.ToInt32(Console.ReadLine());
+
+//        Console.WriteLine("Dikdörtgenler prizmasının hacmi: " + a * b * c);
+
+
+//    }
+//}
+
+
+
+
+
+
+
